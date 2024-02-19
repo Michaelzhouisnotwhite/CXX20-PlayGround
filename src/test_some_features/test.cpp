06638c987e2c05c6ca87761e3c158badc4c4b84c
Michaelzhouisnotwhite <zyqing601@163.com>
2024-02-19 19:51:37 +0800

    update

---
+++ src/test_some_features/test.cpp
@@ -0,0 +1,24 @@
+#include <fmt/core.h>
+#include <toy/type.h>
+#include <backward.hpp>
+#include <coroutine>
+#include <functional>
+#include <iostream>
+#include <string>
+#ifndef WIN32
+#define BACKWARD_HAS_DW 1
+#include "backward.hpp"
+#endif
+#include <nlohmann/json.hpp>
+#include <ranges>
+int main(int args, char* argv[]) {
+    std::vector<int> v(100);
+    for (int i : std::views::iota(1, 10) | std::views::filter([](int i) {
+                     return i % 2 == 0;
+                 }) | std::views::transform([](int i) {
+                     return i + 1;
+                 })) {
+        fmt::println("{}", i);
+    }
+    return 0;
+}
